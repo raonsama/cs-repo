c1dac20ebe475a1fb0cb243870d5120775fb5ec9
Raymond M <raonsama@proton.me>
2024-10-13 06:04:59 +0800

    Update Gomov

--- Gomov/src/main/kotlin/com/hexated/GomovPlugin.kt
+++ Gomov/src/main/kotlin/com/hexated/GomovPlugin.kt
@@ -9,10 +9,10 @@ import android.content.Context
 class GomovPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Gomov())
-        registerMainAPI(DutaMovie())
-        registerMainAPI(Ngefilm())
-        registerMainAPI(Nodrakorid())
+        // registerMainAPI(Gomov())
+        // registerMainAPI(DutaMovie())
+        // registerMainAPI(Ngefilm())
+        // registerMainAPI(Nodrakorid())
         registerMainAPI(Pusatfilm())
         registerExtractorAPI(FilelionsTo())
         registerExtractorAPI(Likessb())
