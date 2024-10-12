a49d3d31df202410726dc2fcb17c0f09e24ff514
Raymond M <raonsama@proton.me>
2024-10-12 12:57:39 +0800

    🎉 Initial

---
+++ Dramaid/src/main/kotlin/com/hexated/Oppadrama.kt
@@ -0,0 +1,8 @@
+package com.hexated
+
+import com.lagradost.cloudstream3.extractors.Filesim
+
+class Oppadrama : DramaidProvider() {
+    override var mainUrl = "https://oppadrama.biz"
+    override var name = "Oppadrama"
+}
